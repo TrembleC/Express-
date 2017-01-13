f11ce703cc909b610e0597e13892c2dec91511c9
qianqc <qianqc@minstone.com.cn7>
2017-01-13 11:22:08 +0800

    更新

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Express-
 
-demo for express<br>
+demo for express<br />
 
 Set up a simple node framework
 
 Update:
-
-2017/1/12	配置requireJS
+<h2>工作安排</h2>
+<ul>
+	<li>配置路由</li>
+	<li>配置requireJS</li>
+	<li>安装supervisor全局监控</li>
+</ul>
--- app.js
+++ app.js
@@ -4,25 +4,19 @@ var path = require("path");
 
 var app = express();
 
-var router = express.Router();
-// 设置视图目录
-// app.set('views','./Express-/views');
-// 设置默认的模版引擎
-
-// app.engine('ejs', engine);
-// app.set('views',__dirname + '/views');
-// app.set('views engine', 'ejs');
 // //布尔值设置
 // app.enable('trust proxy');
 
-//默认路径地址
-// app.use(express.static(path.join(__dirname, 'public')));
-// 
+// 默认路径地址
+app.use(express.static(path.join(__dirname, 'public')));	//使用静态资源的默认路径，路径可以省略默认是'/'
+
 app.engine('ejs', ejsMate);
 
-app.set('views',__dirname + '/views');
-app.set('view engine', 'ejs'); // so you can render('index')
+//设置模版文件夹
+app.set('views',__dirname + '/views');	//__dirname当前执行js文件路径，设置视图目录
+app.set('view engine', 'ejs'); 			
 
+//测试例子
 app.get("/", function(req, res){
 	res.render("index", {
 		title: 'test',
